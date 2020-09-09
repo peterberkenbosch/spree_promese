f280768223067dbd082cd47196e980c82ad69934
Fabian Oudhaarlem <fabian@oldharlem.nl>
2020-09-09 16:52:48 +0200

    Article serializer first setup

---
+++ app/serializers/promese_serializer.rb
@@ -0,0 +1,20 @@
+class PromeseSerializer
+  attr_reader :record
+
+  def initialize(record)
+    @record = record
+  end
+
+  def to_h
+    serialize
+  end
+
+  def to_json
+    serialize.to_json
+  end
+
+  def serialize
+    raise('serialize method needs to be defined in a promese serializer subclass')
+  end
+
+end
