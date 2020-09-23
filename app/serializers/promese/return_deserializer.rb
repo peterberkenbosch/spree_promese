be29c44f56064f267b2b8191dcc61a2ec2a2de6e
Fabian Oudhaarlem <fabian@oldharlem.nl>
2020-09-23 15:45:36 +0200

    dont use return quantity

--- app/serializers/promese/return_deserializer.rb
+++ app/serializers/promese/return_deserializer.rb
@@ -30,14 +30,20 @@ class Promese::ReturnDeserializer < PromeseDeserializer
     begin
       @order = Spree::Order.friendly.find(data['shipmentId'])
       returned_line_items = data['returnItems'].each_with_object({}) do |return_item_data, hash|
-        return if return_item_data['returnStatus'] != 'GOOD'
+        next if return_item_data['returnStatus'] != 'GOOD'
         hash[order.line_items.joins(:variant).find_by(spree_variants: {sku: return_item_data['eanCode']})] = return_item_data['quantity']
       end.compact
       default_stock_location = Spree::StockLocation.order_default.first
 
       ra = order.return_authorizations.create(stock_location: default_stock_location, reason: Spree::ReturnAuthorizationReason.first)
       returned_line_items.each do |line_item, returned_quantity|
-        ra.return_items.create(inventory_unit: order.inventory_units.find_by(line_item_id: line_item.id, state: :shipped), return_quantity: returned_quantity)
+        if Spree::ReturnItem.new.respond_to?(:return_quantity)
+          ra.return_items.create(inventory_unit: order.inventory_units.find_by(line_item_id: line_item.id, state: :shipped), return_quantity: returned_quantity)
+        else
+          returned_quantity.times do
+            ra.return_items.create(inventory_unit: order.inventory_units.find_by(line_item_id: line_item.id, state: :shipped))
+          end
+        end
       end
       cr = Spree::CustomerReturn.new(stock_location: default_stock_location)
       cr.return_items = ra.return_items
@@ -46,6 +52,8 @@ class Promese::ReturnDeserializer < PromeseDeserializer
       ri = Spree::Reimbursement.build_from_customer_return(cr)
       ri.save!
       ri.perform!
+
+      logger.info "Imported returns for order #{@order.number}. Returned items: #{returned_line_items.keys.map {|l| l.sku}.to_sentence}"
     rescue StandardError => e
       logger.error "Something went wrong while importing a return for order #{data['shipmentId']}"
       logger.error e.message
